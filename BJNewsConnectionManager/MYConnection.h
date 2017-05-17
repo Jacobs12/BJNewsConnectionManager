e8cd49eafe7fe630fdfd622f20bd166abf26b752
Jacobs12 <1440913385@qq.com>
2017-05-17 13:59:38 +0800

    update

---
+++ BJNewsConnectionManager/MYConnection.h
@@ -0,0 +1,13 @@
+//
+//  MYConnection.h
+//  BJNewsConnectionManager
+//
+//  Created by wolffy on 2017/5/17.
+//  Copyright © 2017年 灰太狼. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MYConnection : NSObject
+
+@end
