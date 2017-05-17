94b0b6c0be8d37b8c0fc735c14a2a76347eaed89
Jacobs12 <1440913385@qq.com>
2017-05-17 17:50:06 +0800

    delete nsurlconnection

--- BJNewsConnectionManager/MYConnection/FTSessionRequest.m
+++ BJNewsConnectionManager/MYConnection/FTSessionRequest.m
@@ -13,12 +13,12 @@
 @end
 
 @implementation FTSessionRequest
+/*
+ 一般模式（default）：工作模式类似于原来的NSURLConnection，可以使用缓存的Cache，Cookie，鉴权。
+ 及时模式（ephemeral）：不使用缓存的Cache，Cookie，鉴权。
+ 后台模式（background）：在后台完成上传下载，创建Configuration对象的时候需要给一个NSString的ID用于追踪完成工作的Session是哪一个
+ */
 - (void)startRequest{
-    /*
-     一般模式（default）：工作模式类似于原来的NSURLConnection，可以使用缓存的Cache，Cookie，鉴权。
-     及时模式（ephemeral）：不使用缓存的Cache，Cookie，鉴权。
-     后台模式（background）：在后台完成上传下载，创建Configuration对象的时候需要给一个NSString的ID用于追踪完成工作的Session是哪一个
-     */
     NSURLSessionConfiguration * ephemralConfigObject = [NSURLSessionConfiguration ephemeralSessionConfiguration];
     NSURLSession * ephemralSession = [NSURLSession sessionWithConfiguration:ephemralConfigObject delegate:self delegateQueue:[NSOperationQueue mainQueue]];
     NSURL * url = [NSURL URLWithString:self.url];
@@ -30,7 +30,6 @@
                 self.failedBlock();
             }
         }else{
-//            NSLog(@"使用session下载成功");
             if(self.finishedBlock){
                 self.finishedBlock(data,response);
             }
